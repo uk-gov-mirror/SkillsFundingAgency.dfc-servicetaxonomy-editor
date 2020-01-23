22f1a3d62e6d1a819010ef5d20e084e3ff19b8b0
Phil Davies <github@phildavies.co.uk>
2020-01-23 11:10:30 +0000

    wip

--- DFC.ServiceTaxonomy.GraphSync/GraphSyncers/BagPartGraphSyncer.cs
+++ DFC.ServiceTaxonomy.GraphSync/GraphSyncers/BagPartGraphSyncer.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+// using Microsoft.Extensions.DependencyInjection;
+// using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata.Models;
 using OrchardCore.Flows.Models;
 
@@ -9,6 +11,12 @@ namespace DFC.ServiceTaxonomy.GraphSync.GraphSyncers
     public class BagPartGraphSyncer : IContentPartGraphSyncer
     {
         public string? PartName => nameof(BagPart);
+        private readonly IServiceProvider _serviceProvider;
+
+        public BagPartGraphSyncer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
 
         public Task AddSyncComponents(
             dynamic graphLookupContent,
@@ -16,6 +24,14 @@ namespace DFC.ServiceTaxonomy.GraphSync.GraphSyncers
             IDictionary<(string destNodeLabel, string destIdPropertyName, string relationshipType), IEnumerable<string>> nodeRelationships,
             ContentTypePartDefinition contentTypePartDefinition)
         {
+            // foreach (ContentItem contentItem in graphLookupContent.ContentItems)
+            // {
+            //     IGraphSyncer graphSyncer = _serviceProvider.GetService<IGraphSyncer>();
+            //     graphSyncer.SyncToGraph(contentItem);
+            //     //todo: need to create relationship to each node created (embedded content item had a graph sync)
+            //     // get SyncToGraph to return
+            // }
+            // return Task.CompletedTask;
             throw new NotImplementedException();
         }
     }
