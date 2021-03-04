930ffb49b6c62409614b92f3f8ac5eddbb391b38
Prasad Ramireddy <pramireddy08@gmail.com>
2021-03-04 16:15:54 +0000

    FT2-256 Added new Media module

---
+++ DFC.ServiceTaxonomy.Media/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "DFC.ServiceTaxonomy.Media",
+    Author = "National Careers Service",
+    Website = "https://github.com/SkillsFundingAgency/dfc-servicetaxonomy-editor",
+    Version = "0.0.1",
+    Description = "DFC.ServiceTaxonomy.Media",
+    Category = "Content Management"
+)]
